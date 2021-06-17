ad5e549c88d342766509ee88e3033a051bdaccea
刺客 <zz225575@alibaba-inc.com>
2021-06-17 17:40:02 +0800

    fix:UISegmentedControl crash

--- NUI/UI/UISegmentedControl+NUI.m
+++ NUI/UI/UISegmentedControl+NUI.m
@@ -26,12 +26,12 @@
     self.nuiApplied = YES;
 }
 
-- (void)override_UISearchBar_didMoveToWindow
+- (void)override_UISegmentedControl_didMoveToWindow
 {
     if (!self.isNUIApplied) {
         [self applyNUI];
     }
-    [self override_UISearchBar_didMoveToWindow];
+    [self override_UISegmentedControl_didMoveToWindow];
 }
 
 @end
